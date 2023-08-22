48cc2dabb62ab3d4086d6c43fcb3ead6c6ca4b83
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-08-23 01:18:48 +0300

    completed deliverables

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,2 +1,30 @@
 class LeasesController < ApplicationController
+    def create
+        lease = Lease.create(lease_params)
+        if lease.valid?
+            render json: lease, status: :ok
+        else
+            render json: {error: lease.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        lease = Lease.find_by(id: params[:id])
+        if lease
+            lease.destroy
+            head :no_content
+        else
+            render_not_found
+        end
+    end
+
+    private
+
+    def render_not_found
+        render json: {error: "Lease not found"}, status: :not_found
+    end
+
+    def lease_params
+        params.permit(:rent, :apartment_id, :tenant_id)
+    end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,2 +1,65 @@
 class TenantsController < ApplicationController
+
+    def index
+        tenants = Tenant.all
+        render json: tenants, status: :ok
+    end
+
+    def show
+        tenant = find_by_id
+        if tenant
+            render json: tenant, status: :ok
+        else
+            render_not_found
+        end
+    end
+
+    def create
+        tenant = Tenant.create(tenant_params)
+        if tenant.valid?
+            render json: tenant, status: :ok
+        else
+            render json: {error: tenant.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        tenant = find_by_id
+        if tenant
+            tenant.update(tenant_params)
+            if tenant.valid?
+                render json: tenant, status: :ok
+            else
+                render json: {error: tenant.errors.full_messages}, status: :unprocessable_entity
+            end
+        else
+            render_not_found
+        end
+    end
+
+    def destroy
+        tenant = find_by_id
+        if tenant
+            tenant.destroy
+            head :no_content
+        else
+            render_not_found
+        end
+    end
+
+    private
+
+    def tenant_params
+        params.permit(:name, :age)
+    end
+
+    def render_not_found
+        render json: {error: "Tenant not found"}, status: :not_found
+    end
+
+    def find_by_id
+        Tenant.find_by(id: params[:id])
+    end
 end
+
+
